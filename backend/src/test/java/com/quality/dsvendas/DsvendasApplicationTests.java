6876574ea5d2726ac0207151070b0f134335039a
Livio Neiva <livio_neiva@hotmail.com>
2021-05-03 21:10:26 -0300

    project Created

---
+++ backend/src/test/java/com/quality/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quality.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
