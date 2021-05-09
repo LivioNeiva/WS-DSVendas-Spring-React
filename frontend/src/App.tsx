78ceee1a4e40e2cdcfcfe0f016400d3a9370266a
Livio Neiva <livio_neiva@hotmail.com>
2021-05-09 18:42:05 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import DataTable from "components/dataTable";
+import Footer from "components/footer";
+import NavBar from "components/navBar";
 
-function App() {
+{/* todo return desse arquivo será exibido no naegador(browse) */}
+function App() {  {/* pode ser function ou constant */}
   return (
-    <div>
+    <> {/* (fragments) todo os elementos de retorno ficara entre as duas tags */}
+    <NavBar />
+    <div className="container">{/* container bootStrap para alinar infor da div  */}
       <h1 className="text-primary">Ola mundo!!!</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
 export default App;
+
+{/*  */}
---
+++ frontend/src/components/dataTable/index.tsx
@@ -0,0 +1,82 @@
+
+function DataTable() { {/* pode ser function ou constant */ }
+    
+    return (
+        <div className="table-responsive"> {/* class bootStrap */}
+            <table className="table table-striped table-sm"> {/* class bootStrap */}
+                <thead> {/* A tag <thead> apresenta a funcionalidade de agrupar elementos do conteúdo de um cabeçalho no HTML. Ela deve ser usada como filho de um elemento <table>, ou seja, estar após <table>, e antes de qualquer <tbody>, <tr> ou <tfoot> */}
+                    <tr> {/* A etiqueta <tr> que significa table row é usada para definir uma linha. */}
+                        <th>Data</th> {/* define uma célula cabeçalho do grupo de células de sua tabela */}
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody> {/* tag é usada para formar uma combinação de conteúdo do corpo HTML */}
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td> {/* A etiqueta <td> que significa table data é usada para definir uma célula. */}
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    );
+
+}
+export default DataTable;
+
+{/*  */}
