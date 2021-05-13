c46b4164801ef13ce0f94b24b8a0131723217963
Livio Neiva <livio_neiva@hotmail.com>
2021-05-13 02:03:00 -0300

    Domain model database seed

---
+++ backend/README.md
@@ -0,0 +1,29 @@
+﻿# DashBoard de Vendas
+
+Objetivos do projeto
+
+Implementar o back end
+
+    Modelo de domínio
+    Estruturar o back end no padrão camadas
+    Consulta paginada de vendas
+    Consultas agrupadas para gráficos
+    Implantação na nuvem
+
+Entendendo o modelo de dados
+
+https://github.com/LivioNeiva/WS-DSVendas-Spring-React/blob/main/dados.xlsx
+
+Passo 1: configuração de segurança
+ -COMMIT: Security config
+
+Passo 2: criar as entidades e o seed do banco
+
+ 
+
+Implementamos:
+	application.properties
+	Seed do banco de dados
+
+COMMIT: Domain model, database seed
+
---
+++ backend/src/main/java/com/quality/dsvendas/entities/Sale.java
@@ -0,0 +1,84 @@
+package com.quality.dsvendas.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //informa ao banco esse id é auto incrementavel
+	private long id;
+	private Integer visited;
+	private Integer deals; //vendas fechadas
+	private Double amount;
+	private LocalDate date;
+	
+	@Autowired
+	@ManyToOne //muito p/ um, a chave estrangeira fica no lado do muitos, nessa caso e sale
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+	
+	public Sale() {
+		
+	}
+
+	public Sale(long id, Integer visited, Integer deals, Double aumont, LocalDate date) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = aumont;
+		this.date = date;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+}
